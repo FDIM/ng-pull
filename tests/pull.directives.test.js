d6e38f2649a21eed4b63c7ae7f2611b5f9930962
Domas Trijonis <domas.trijonis@maersk.com>
2016-02-25 16:40:16 +0100

    reverted few changes + 100% test coverage

--- tests/pull.directives.test.js
+++ tests/pull.directives.test.js
@@ -5,7 +5,7 @@
  * build process will exclude all `.spec.js` files from the build
  * automatically.
  */
-describe('pull directives', function () {
+describe('pull', function () {
   beforeEach(module("ngPull"));
 
   var $compile,
@@ -21,15 +21,16 @@ describe('pull directives', function () {
     pullService = _ngPullService_;
   }));
 
-  describe("horizontal", function () {
+  describe("horizontal directives", function () {
     var template = [
-            '<div style="width:100px; height:100px;" on-pull-left="false" on-pull-right="false" pull-right-disabled="rightDisabled">',
+            '<div style="width:100px; height:100px;" pull-left-timeout="{{timeout}}" on-pull-left="onPull($reset)" on-pull-right="false" pull-right-disabled="rightDisabled">',
               '<div pull-left-container></div>',
               '<div pull-target></div>',
               '<div pull-right-container></div>',
             '</div>'].join('');
 
     it("should init", function () {
+      $rootScope.timeout = 200;
       var element = $compile(template)($rootScope);
       $rootScope.rightDisabled = true;
       $rootScope.$digest(); // process all directives
@@ -43,44 +44,114 @@ describe('pull directives', function () {
     });
 
     it("should ignore all other buttons except left", function() {
+      $rootScope.timeout = 200;
       var element = $compile(template)($rootScope);
       $rootScope.$digest();
       $rootScope.$pullLeftController.suspended = false;
-      element.triggerHandler(new MouseEvent("mousedown",{clientX:10, clientY:20, button:1}));
-      element.triggerHandler(new MouseEvent("mousedown",{clientX:10, clientY:20, button:2}));
-      //expect($rootScope.$pullLeftController.suspended).toEqual(false);
-      //expect($rootScope.$pullRightController.suspended).toEqual(false);
+      element[0].dispatchEvent(new MouseEvent("mousedown",{clientX:10, clientY:20, button:1}));
+      element[0].dispatchEvent(new MouseEvent("mousedown",{clientX:10, clientY:20, button:2}));
+      $rootScope.$digest();
+      expect($rootScope.$pullLeftController.suspended).toEqual(false);
+      expect($rootScope.$pullRightController.suspended).toEqual(false);
     });
-    it("should respond to events", function() {
+    it("should respond to events and invoke pull-left callback", function() {
+      $rootScope.timeout = 200;
       var element = $compile(template)($rootScope);
       var target = angular.element($window);
+      var pulledLeft = false;
+      $rootScope.onPull = function(){
+        pulledLeft = true;
+      };
       $rootScope.$digest();
       $rootScope.$pullLeftController.suspended = false;
-      element.triggerHandler(new MouseEvent("mousedown",{clientX:10, clientY:20, button:0}));
+      element[0].dispatchEvent(new MouseEvent("mousedown",{clientX:200, clientY:20, button:0}));
       expect($rootScope.$pullLeftController.suspended).toEqual(true);
       expect($rootScope.$pullRightController.suspended).toEqual(true);
       $rootScope.$digest();
-      target.triggerHandler(new MouseEvent("mousemove",{clientX:14, clientY:20, button:0}));
-      $rootScope.$digest();
-      //console.info($rootScope.$pullLeftProgress);
-      //expect($rootScope.$pullRightProgress>0).toEqual(true);
-      target.triggerHandler(new MouseEvent("mousemove",{clientX:24, clientY:20, button:0}));
-      $rootScope.$digest();
-      target.triggerHandler(new MouseEvent("mousemove",{clientX:50, clientY:20, button:0}));
-      $rootScope.$digest();
-      target.triggerHandler(new MouseEvent("mousemove",{clientX:90, clientY:20, button:0}));
-      $rootScope.$digest();
-      target.triggerHandler(new MouseEvent("mousemove",{clientX:100, clientY:20, button:0}));
-      $rootScope.$digest();
-      target.triggerHandler(new MouseEvent("mouseup",{clientX:100, clientY:20, button:0}));
-      $rootScope.$digest();
+      target[0].dispatchEvent(new MouseEvent("mousemove",{clientX:180, clientY:20, button:0}));
+      pullService.$$rAF.flush();
+      expect($rootScope.$pullLeftProgress>0).toEqual(true);
+      target[0].dispatchEvent(new MouseEvent("mousemove",{clientX:150, clientY:20, button:0}));
+      pullService.$$rAF.flush();
+      expect($rootScope.$pullLeftProgress===50).toEqual(true);
+      target[0].dispatchEvent(new MouseEvent("mousemove",{clientX:130, clientY:20, button:0}));
+      pullService.$$rAF.flush();
+      target[0].dispatchEvent(new MouseEvent("mousemove",{clientX:120, clientY:20, button:0}));
+      pullService.$$rAF.flush();
+      target[0].dispatchEvent(new MouseEvent("mousemove",{clientX:110, clientY:20, button:0}));
+      pullService.$$rAF.flush();
+      target[0].dispatchEvent(new MouseEvent("mousemove",{clientX:100, clientY:20, button:0}));
+      pullService.$$rAF.flush();
+      expect($rootScope.$pullLeftProgress).toEqual(100);
+      target[0].dispatchEvent(new MouseEvent("mousemove",{clientX:80, clientY:20, button:0}));
+      pullService.$$rAF.flush();
+      expect($rootScope.$pullLeftProgress).toEqual(100);
+      target[0].dispatchEvent(new MouseEvent("mouseup",{clientX:80, clientY:20, button:0}));
+      pullService.$$rAF.flush();
+      expect($rootScope.$pullLeftProgress).toEqual(0);
 
+      expect(pulledLeft).toBe(true);
+    });
+    it("should respond to events, cancel gesture upon move due to timeout and not invoke pull-left callback", function() {
+      $rootScope.timeout = -1;
+      var element = $compile(template)($rootScope);
+      var target = angular.element($window);
+      var pulledLeft = false;
+      $rootScope.onPull = function(){
+        pulledLeft = true;
+      };
+      $rootScope.$digest();
+      $rootScope.$pullLeftController.suspended = false;
+      element[0].dispatchEvent(new MouseEvent("mousedown",{clientX:200, clientY:20, button:0}));
+      expect($rootScope.$pullLeftController.suspended).toEqual(true);
+      expect($rootScope.$pullRightController.suspended).toEqual(true);
+      $rootScope.$digest();
+      target[0].dispatchEvent(new MouseEvent("mousemove",{clientX:199, clientY:20, button:0}));
+      pullService.$$rAF.flush();
+      expect($rootScope.$pullLeftProgress).toEqual(0);
+      target[0].dispatchEvent(new MouseEvent("mousemove",{clientX:158, clientY:20, button:0})); // should not affect progress
+      pullService.$$rAF.flush();
+      expect($rootScope.$pullLeftProgress).toEqual(0);
+      target[0].dispatchEvent(new MouseEvent("mouseup",{clientX:80, clientY:20, button:0}));
+      pullService.$$rAF.flush();
+      expect($rootScope.$pullLeftProgress).toEqual(0);
+
+      expect(pulledLeft).toBe(false);
     });
 
+    it("should respond to events, cancel gesture upon move due to timeout, remove no-select class and not invoke pull-left callback", function() {
+      $rootScope.timeout = -1;
+      // modify template and remove pull right gesture
+      var element = $compile(template.replace('on-pull-right="false" pull-right-disabled="rightDisabled"','').replace('pull-right-container',''))($rootScope);
+      var target = angular.element($window);
+      var pulledLeft = false;
+      $rootScope.onPull = function(){
+        pulledLeft = true;
+      };
+      $rootScope.$digest();
+      $rootScope.$pullLeftController.suspended = false;
+      element[0].dispatchEvent(new MouseEvent("mousedown",{clientX:200, clientY:20, button:0}));
+      expect($rootScope.$pullLeftController.suspended).toEqual(true);
+      expect($rootScope.$pullRightController).not.toBeDefined();
+      $rootScope.$digest();
+      target[0].dispatchEvent(new MouseEvent("mousemove",{clientX:199, clientY:20, button:0}));
+      pullService.$$rAF.flush();
+      expect($rootScope.$pullLeftProgress).toEqual(0);
+      target[0].dispatchEvent(new MouseEvent("mousemove",{clientX:158, clientY:20, button:0})); // should not affect progress nor reach target
+
+      expect(function(){pullService.$$rAF.flush();}).toThrowError("No rAF callbacks present");
+
+      expect($rootScope.$pullLeftProgress).toEqual(0);
+      target[0].dispatchEvent(new MouseEvent("mouseup",{clientX:80, clientY:20, button:0})); // should not affect progress nor reach target
+      expect(function(){pullService.$$rAF.flush();}).toThrowError("No rAF callbacks present");
+      expect($rootScope.$pullLeftProgress).toEqual(0);
+
+      expect(pulledLeft).toBe(false);
+    });
   });
-  describe("vertical", function () {
+  describe("vertical directives", function () {
     var template = [
-            '<div on-pull-up="false" on-pull-down="false" pull-up-disabled="upDisabled">',
+            '<div on-pull-up="onPull($reset)" on-pull-down="false">',
               '<div pull-down-container></div>',
               '<div pull-target></div>',
               '<div pull-up-container></div>',
@@ -95,8 +166,50 @@ describe('pull directives', function () {
       $rootScope.upDisabled = true;
       $rootScope.$pullUpController.suspended = true;
       $rootScope.$digest();
-      pullService.$$rAF.flush()
+    });
+    it("should respond to events and invoke pull-up callback", function() {
+      var element = $compile(template)($rootScope);
+      var target = angular.element($window);
+      var pulledUp = false;
+      var resetFn;
+      $rootScope.onPull = function($reset){
+        pulledUp = true;
+        resetFn = $reset;
+        return false;
+      };
       $rootScope.$digest();
+      $rootScope.$pullUpController.suspended = false;
+      element[0].dispatchEvent(new MouseEvent("mousedown",{clientY:200, clientX:20, button:0}));
+      expect($rootScope.$pullUpController.suspended).toEqual(true);
+      expect($rootScope.$pullDownController.suspended).toEqual(true);
+      $rootScope.$digest();
+      target[0].dispatchEvent(new MouseEvent("mousemove",{clientY:180, clientX:20, button:0}));
+      pullService.$$rAF.flush();
+      expect($rootScope.$pullUpProgress>0).toEqual(true);
+      target[0].dispatchEvent(new MouseEvent("mousemove",{clientY:150, clientX:20, button:0}));
+      pullService.$$rAF.flush();
+      expect($rootScope.$pullUpProgress===50).toEqual(true);
+      target[0].dispatchEvent(new MouseEvent("mousemove",{clientY:130, clientX:20, button:0}));
+      pullService.$$rAF.flush();
+      target[0].dispatchEvent(new MouseEvent("mousemove",{clientY:120, clientX:20, button:0}));
+      pullService.$$rAF.flush();
+      target[0].dispatchEvent(new MouseEvent("mousemove",{clientY:110, clientX:20, button:0}));
+      pullService.$$rAF.flush();
+      target[0].dispatchEvent(new MouseEvent("mousemove",{clientY:100, clientX:20, button:0}));
+      pullService.$$rAF.flush();
+      expect($rootScope.$pullUpProgress).toEqual(100);
+      target[0].dispatchEvent(new MouseEvent("mousemove",{clientY:80, clientX:20, button:0}));
+      pullService.$$rAF.flush();
+      expect($rootScope.$pullUpProgress).toEqual(100);
+      target[0].dispatchEvent(new MouseEvent("mouseup",{clientY:80, clientX:20, button:0}));
+      pullService.$$rAF.flush();
+      expect($rootScope.$pullUpProgress).toEqual(100); // progress is reset once $reset in invoked
+
+      expect(pulledUp).toBe(true);
+
+      resetFn();
+      pullService.$$rAF.flush();
+      expect($rootScope.$pullUpProgress).toEqual(0);
     });
 
   });
